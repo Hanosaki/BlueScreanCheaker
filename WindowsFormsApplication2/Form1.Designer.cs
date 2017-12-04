0e5afaf6dccad0f71b0fba7fdcad050decd0fce9
Hanosaki <suikyouinga@gmail.com>
2017-12-04 16:37:51 +0900

    firstCommit

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // testLabel
+            // 
+            this.testLabel.AutoSize = true;
+            this.testLabel.Location = new System.Drawing.Point(12, 33);
+            this.testLabel.Name = "testLabel";
+            this.testLabel.Size = new System.Drawing.Size(35, 12);
+            this.testLabel.TabIndex = 0;
+            this.testLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.testLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label testLabel;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management.Automation;
+using System.Collections;
+using System.IO;
+using System.Management.Automation.Runspaces;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            using (var tmp = new RunspaceInvoke())
+            {
+                Label label = testLabel;
+                
+                string getSytemLog = @"Get-EventLog System";
+                string getAppLog = @"Get-EventLog Application";
+
+                var systemLog = tmp.Invoke(getSytemLog, new object[] { });
+                var appLog = tmp.Invoke(getAppLog, new object[] { });
+
+                var path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                var writer = new StreamWriter(path + "\\timeKP41.csv", false, UTF8Encoding.UTF8);
+
+                ArrayList info = new ArrayList();
+                string time = "0";
+
+                foreach (var r in systemLog)
+                {
+                    var id = r.Properties["EventID"].Value.ToString();
+                    if (id == "41")
+                    {
+                        time = r.Properties["TimeGenerated"].Value.ToString().Remove(10);
+
+                        info.Add("--------(" + time + ")----------");
+                        info.Add("--------ApplicationLog----------");                        
+                        foreach (var m in appLog)
+                        {
+                            if (m.Properties["TimeGenerated"].Value.ToString().Remove(10) == time)
+                            {
+                                //同じ時間が見つかった場合の処理
+                                if (!m.Properties["EntryType"].Value.ToString().Equals("Information")
+                                    && !m.Properties["EntryType"].Value.ToString().Equals("0"))
+                                info.Add(m.Properties["EventID"].Value.ToString() +
+                                ","
+                                + m.Properties["Message"].Value.ToString());
+                            }
+                        }
+
+                        info.Add("---------SystemLog-----------");
+
+                    }
+
+                    if (id != "41" && r.Properties["TimeGenerated"].Value.ToString().Remove(10) == time)
+                    {
+                        //ここで，イベントIDとメッセージを受け取る
+                        if (!r.Properties["EntryType"].Value.ToString().Equals("Information"))
+                        info.Add(r.Properties["EventID"].Value.ToString() +
+                            "," 
+                            +r.Properties["Message"].Value.ToString());
+                    }
+
+                    
+                }
+                char[] removeCharas = new char[] { '\r', '\n' };
+                string[] str = new string[info.Count];
+                writer.WriteLine("EventID,Message");
+                int count = 0;
+                foreach (var i in info)
+                {
+                    str[count] = i.ToString();
+                    ++count;
+                }
+
+                for (int i = 0; i < str.Length;++i )
+                {
+                    foreach (char c in removeCharas)
+                        str[i] = str[i].Replace(c.ToString(), "");
+                    writer.WriteLine(str[i]);
+                }
+
+                    writer.Close();
+
+            }
+            
+        }
+    }
+}
