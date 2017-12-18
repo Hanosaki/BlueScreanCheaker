e7065b7a769df6412d7e03731ce80664df10e5ed
Hanosaki <suikyouinga@gmail.com>
2017-12-18 19:24:17 +0900

    サンプルログを読み込む用に変更

--- WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/Form1.cs
@@ -23,6 +23,15 @@ namespace WindowsFormsApplication2
         {
             InitializeComponent();
 
+            ArrayList info = new ArrayList();
+            ArrayList application_ids = new ArrayList();
+            ArrayList system_ids = new ArrayList();
+
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            var writer = new StreamWriter(path + "\\timeKP41.csv", false, UTF8Encoding.UTF8);
+
+            string id = "0";
+
             try
             {
 
@@ -30,27 +39,25 @@ namespace WindowsFormsApplication2
                 {
                     Label label = testLabel;
 
-                    string getSytemLog = @"Get-EventLog System";
-                    string getAppLog = @"Get-EventLog Application";
+                    var sr = new StreamReader(@"sampleApplication.txt", System.Text.Encoding.GetEncoding("shift-jis"));
+                    var taskApp = sr.ReadLine();
+                    sr.Close();
 
-                    var systemLog = tmp.Invoke(getSytemLog, new object[] { });
-                    var appLog = tmp.Invoke(getAppLog, new object[] { });
+                    sr = new StreamReader(@"sampleSystem.txt", System.Text.Encoding.GetEncoding("shift-jis"));
+                    var taskSytem = sr.ReadLine();
+                    sr.Close();
 
-                    var path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-                    var writer = new StreamWriter(path + "\\timeKP41.csv", false, UTF8Encoding.UTF8);
-
-                    ArrayList info = new ArrayList();
-                    ArrayList application_ids = new ArrayList();
-                    ArrayList system_ids = new ArrayList();
+                    var systemLog = tmp.Invoke(taskSytem, new object[] { });
+                    var appLog = tmp.Invoke(taskApp, new object[] { });
 
                     string time = "0";
 
                     foreach (var r in systemLog)
                     {
-                        var id = r.Properties["EventID"].Value.ToString();
+                        id = r.Properties["Id"].Value.ToString();
                         if (id == "41")
                         {
-                            time = r.Properties["TimeGenerated"].Value.ToString().Remove(10);
+                            time = r.Properties["TimeCreated"].Value.ToString().Remove(10);
 
                             info.Add("--------(" + time + ")----------");
                             info.Add("--------ApplicationLog----------");
@@ -63,23 +70,25 @@ namespace WindowsFormsApplication2
 
                             foreach (var m in appLog)
                             {
-                                if (m.Properties["TimeGenerated"].Value.ToString().Remove(10) == time)
+                                //特定のログから読み込む場合は，IDが違うので注意！
+                                if (m.Properties["TimeCreated"].Value.ToString().Remove(10) == time)
                                 {
                                     //同じ時間が見つかった場合の処理
-                                    if (!m.Properties["EntryType"].Value.ToString().Equals("Information"))
+                                    if (!m.Properties["Level"].Value.ToString().Equals("4"))
                                     {
 
-                                        if (!m.Properties["EntryType"].Value.ToString().Equals("0"))
+                                        if (!m.Properties["Level"].Value.ToString().Equals("0") &&
+                                            !m.Properties["Id"].Value.ToString().Equals("0"))
                                         {
-                                            info.Add(m.Properties["EventID"].Value.ToString() +
+                                            info.Add(m.Properties["Id"].Value.ToString() +
                                             ","
                                             + m.Properties["Message"].Value.ToString());
 
-                                            string[] row_3 = { m.Properties["EventID"].Value.ToString(),
+                                            string[] row_3 = { m.Properties["Id"].Value.ToString(),
                                                          m.Properties["Message"].Value.ToString() };
 
                                             ErrorListView.Items.Add(new ListViewItem(row_3));
-                                            application_ids.Add(m.Properties["EventID"].Value.ToString());
+                                            application_ids.Add(m.Properties["Id"].Value.ToString());
                                         }
 
                                     }
@@ -96,93 +105,95 @@ namespace WindowsFormsApplication2
 
                         }
 
-                        if (id != "41" && r.Properties["TimeGenerated"].Value.ToString().Remove(10) == time)
+                        if (id != "41" && r.Properties["TimeCreated"].Value.ToString().Remove(10) == time)
                         {
                             //ここで，イベントIDとメッセージを受け取る
-                            if (!r.Properties["EntryType"].Value.ToString().Equals("Information"))
+                            if (!r.Properties["Level"].Value.ToString().Equals("4") &&
+                                !r.Properties["Id"].Value.ToString().Equals("6") &&
+                                !r.Properties["Id"].Value.ToString().Equals("4115"))
                             {
-                                info.Add(r.Properties["EventID"].Value.ToString() +
+                                info.Add(r.Properties["Id"].Value.ToString() +
                                     ","
                                     + r.Properties["Message"].Value.ToString());
 
-                                string[] row_3 = { r.Properties["EventID"].Value.ToString(),
+                                string[] row_3 = { r.Properties["Id"].Value.ToString(),
                                                          r.Properties["Message"].Value.ToString() };
 
                                 ErrorListView.Items.Add(new ListViewItem(row_3));
-                                system_ids.Add(r.Properties["EventID"].Value.ToString());
+                                system_ids.Add(r.Properties["Id"].Value.ToString());
 
                             }
                         }
 
 
                     }
-                    char[] removeCharas = new char[] { '\r', '\n' };
-                    string[] str = new string[info.Count];
-                    writer.WriteLine("EventID,Message");
-                    int count = 0;
-                    foreach (var i in info)
-                    {
-                        str[count] = i.ToString();
-                        ++count;
-                    }
-
-                    for (int i = 0; i < str.Length; ++i)
-                    {
-                        foreach (char c in removeCharas)
-                            str[i] = str[i].Replace(c.ToString(), "");
-                        writer.WriteLine(str[i]);
-                    }            
-                    writer.Close();
-
-                    while(application_ids.Count != 0)
-                    {
-                        var tmpID = application_ids[0].ToString();
-                        int idCount = 0;
-
-                        foreach(var i in application_ids)
-                        {
-                            if(i.ToString() == tmpID )
-                                ++idCount;
-                        }
-
-                        if (counts < idCount)
-                        {
-                            counts = idCount;
-                            MostFrequentlyID_Application = int.Parse(tmpID);
-                        }
-
-                        application_ids.Remove(tmpID);
-
-                    }
-
-                    while (system_ids.Count != 0)
-                    {
-                        var tmpID = system_ids[0].ToString();
-                        int idCount = 0;
-
-                        foreach (var i in system_ids)
-                        {
-                            if (i.ToString() == tmpID)
-                                ++idCount;
-                        }
-
-                        if (counts_system < idCount)
-                        {
-                            counts_system = idCount;
-                            MostFrequentlyID_System = int.Parse(tmpID);
-                        }
-
-                        system_ids.Remove(tmpID);
-
-                    }
-
                     
                 }
 
             }
             catch(Exception e)
             {
-                MessageBox.Show(e.Message);
+                MessageBox.Show(e.Message + "id:" + id);
+            }
+
+            char[] removeCharas = new char[] { '\r', '\n' };
+            string[] str = new string[info.Count];
+            writer.WriteLine("EventID,Message");
+            int count = 0;
+            foreach (var i in info)
+            {
+                str[count] = i.ToString();
+                ++count;
+            }
+
+            for (int i = 0; i < str.Length; ++i)
+            {
+                foreach (char c in removeCharas)
+                    str[i] = str[i].Replace(c.ToString(), "");
+                writer.WriteLine(str[i]);
+            }
+            writer.Close();
+
+            while (application_ids.Count != 0)
+            {
+                var tmpID = application_ids[0].ToString();
+                int idCount = 0;
+
+                foreach (var i in application_ids)
+                {
+                    if (i.ToString() == tmpID)
+                        ++idCount;
+                }
+
+                if (counts < idCount)
+                {
+                    counts = idCount;
+                    MostFrequentlyID_Application = int.Parse(tmpID);
+                }
+
+                application_ids.Remove(tmpID);
+
+            }
+
+            while (system_ids.Count != 0)
+            {
+                var tmpID = system_ids[0].ToString();
+                int idCount = 0;
+
+                foreach (var i in system_ids)
+                {
+                    if (i.ToString() == tmpID)
+                        ++idCount;
+                }
+
+                if (counts_system < idCount)
+                {
+                    counts_system = idCount;
+                    MostFrequentlyID_System = int.Parse(tmpID);
+                }
+
+                system_ids.Remove(tmpID);
+
             }
 
             ErrorListView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
